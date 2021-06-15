15a4acfdd7ca8f836265fc976fc70af876f8978b
Jaehyun Song <tintstyle@gmail.com>
2021-06-15 15:41:46 +0900

    no message

--- ui.R
+++ ui.R
@@ -1,5 +1,5 @@
 library(shiny)
-library(shinythemes)
+library(bslib)
 library(rhandsontable)
 library(PRcalc) # remotes::install_github("JaehyunSong/PRcalc")
 library(tidyverse)
@@ -7,14 +7,15 @@ library(tidyverse)
 # 日本語表示用（ローカルでテスト時はコメントアウト）
 # 起動のたびにフォントダウンロードを行うため、初期起動が重い
 # https://github.com/ltl-manabi/shinyapps.io_japanese_font
-download.file("https://raw.githubusercontent.com/ltl-manabi/shinyapps.io_japanese_font/master/use_ipaex_font.sh", destfile = "use_ipaex_font.sh")
-system("bash ./use_ipaex_font.sh")
+#download.file("https://raw.githubusercontent.com/ltl-manabi/shinyapps.io_japanese_font/master/use_ipaex_font.sh", destfile = "use_ipaex_font.sh")
+#system("bash ./use_ipaex_font.sh")
 
 navbarPage("PRcalc for Web", 
            selected = "比例代表配分計算", 
            collapsible = TRUE, 
            inverse = TRUE, 
-           theme = shinytheme("spacelab"),
+           theme = bs_theme(version = 4, 
+                            bootswatch = "cosmo"),
            
            tabPanel("比例代表配分計算",
                     fluidPage(
